3634a218387202eaef1eb57b89ef452cde4a50d0
keitahiga <76475986+keitahiga@users.noreply.github.com>
2021-10-29 21:04:36 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-/*メニューバーをクリックしたときの動作*/
-$('#header_btn').on('click',(e) => {
-	$('#menu_contents').fadeIn();
-	$('#header_btn').fadeOut();
+/*登録フォームのボタンクリックの動作*/
+$('.signup-show').on('click',(e) => {
+  $('#login').fadeIn();
 });
-/*メニューコンテンツを閉じるときの動作*/
 
 $('#close-modal').on('click',(e) => {
-	$('#menu_contents').fadeOut();
-	$('#header_btn').fadeIn();
+  $('#login').fadeOut();
 });
 
-/*menuボタンを押したときの各々の動作*/
-$('#menu_container li').on('click',(e) => {
-	$('#menu_contents').fadeOut();
-	$('#header_btn').fadeIn();
+/*各foodのボタンを押した時の動作*/
+$('#').on('click',(e) => {
+  $('#login').fadeIn();
+});
+
+$('#').on('click',(e) => {
+  $('#login').fadeOut();
 });
 
 // animatedクラスの付いた要素にwaypointを登録
@@ -36,6 +36,6 @@ $('.animated').waypoint({
     }
   },
 
-  // 要素が表示範囲の30%が画面に来たらhandlerを実行
-  offset: '70%',
+  // 要素が表示範囲の20%が画面に来たらhandlerを実行
+  offset: '80%',
 });
